4b1815eb5cd02ab10d8ab1eac3a5c0264c00140c
MaDladla <cynthiap16dladla@gmail.com>
2024-03-25 20:10:34 +0200

    student portal

--- src/component/RoomCheck.jsx
+++ src/component/RoomCheck.jsx
@@ -13,6 +13,7 @@ function RoomCheck() {
                             <h3 className='md:text-base'>Single Rooms</h3>
                         </div>
                         <div className='flex items-center gap-5'>
+                            
                             <h3 className='text-base'>See All</h3>
                             <svg xmlns="http://www.w3.org/2000/svg" width="15px" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
                         </div>
